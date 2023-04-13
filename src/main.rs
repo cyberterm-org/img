792752ee2bdefcb33f61e0b5f72a75a0c08e4233
Liangdi <wu@liangdi.me>
2023-04-13 17:20:42 +0800

    feat: get data url from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env, fs::File, io::Read, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(path) = env::args().nth(1) {
+        let path = Path::new(&path);
+        let guess = mime_guess::from_path(path);
+
+        let mut file = File::open(&path).unwrap();
+        let mut buffer = Vec::new();
+
+        let _out = file.read_to_end(&mut buffer);
+
+        let _size = match _out {
+            Ok(size) => size,
+            Err(error) => panic!("read file error:{}", error),
+        };
+        let base64 = base64::encode(buffer);
+        let mime_type = guess.first().unwrap();
+        println!("data:image/{};base64,{}", mime_type.to_string(), base64);
+    } else {
+        panic!("You must provide a file path");
+    }
 }
